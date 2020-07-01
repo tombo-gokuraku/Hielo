6ddabfdc2cee7eb5fd25f717c5890432e68ff5f5
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-07-01 17:35:28 +0900

    create Heading component

---
+++ src/components/Heading.js
@@ -0,0 +1,39 @@
+import React from "react"
+import styled from "styled-components"
+import tw, { css } from "twin.macro"
+import breaks from "../styles/styled-breaks"
+// ${tw`flex flex-col items-center justify-center p-8`}
+// min-height: inherit;
+
+function Heading({ title, desc, linkUrl, linkText, hero }) {
+  return (
+    <div
+      css={[
+        css`
+          min-height: inherit;
+        `,
+        tw`flex flex-col items-center justify-center p-8 divide-y`,
+      ]}
+    >
+      <p tw="text-xl text-center text-white uppercase text-opacity-75">
+        {desc}{" "}
+        {linkUrl && linkText && (
+          <a tw="text-white text-opacity-100" href={linkUrl}>
+            {linkText}
+          </a>
+        )}
+      </p>
+      {/* <div tw="w-1/2"></div> */}
+      <Title>{title}</Title>
+    </div>
+  )
+}
+
+const Title = styled.h2`
+  ${tw`w-1/2 text-4xl font-light text-center text-white sm:text-6xl`}
+  ${breaks.greaterThan("md")`
+    font-size: 7rem;
+`}
+`
+
+export default Heading
