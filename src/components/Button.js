a5b29781ac23012ff3f35d771d32d0dd67935e98
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-07-15 17:16:56 +0900

    add animation to button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,22 @@
 import tw from "twin.macro"
 
 const Button = tw.button`
+px-4
+py-2
 bg-white
 border-2
 border-gray-400
 border-solid
 uppercase
-hocus:bg-gray-200
-active:bg-gray-400
-active:border-gray-600
+hover:bg-gray-200
+hover:border-gray-500
+active:bg-gray-500
+active:border-gray-700
 outline-none
 focus:outline-none
+transition
+ease-in-out
+duration-200
 `
 
 export default Button
