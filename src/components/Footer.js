4a188234818057060a9f61b0d9806807e038cf49
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-07-18 17:31:19 +0900

    add Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,51 @@
 import { jsx } from "@emotion/core"
 import "twin.macro"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faTwitter,
+  faInstagram,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons"
+
+const socialLinkData = [
+  {
+    icon: faTwitter,
+    url: "https://twitter.com/home",
+  },
+  {
+    icon: faFacebook,
+    url: "https://www.facebook.com/",
+  },
+  {
+    icon: faInstagram,
+    url: "https://www.instagram.com/",
+  },
+  {
+    icon: faEnvelope,
+    url: "mailto:hogepiyo@example.com",
+  },
+]
+
 function Footer() {
   return (
-    <footer tw="text-gray-500 bg-black">
-      <div css={{ color: "red" }}>icon space</div>
+    <footer tw="flex flex-col items-center justify-center pt-16 pb-8 text-gray-500 bg-black">
+      <ul tw="flex items-center justify-center mb-8">
+        {socialLinkData.map((link, index) => {
+          return (
+            <li key={index}>
+              <a href={link.url} target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon
+                  icon={link.icon}
+                  tw="m-2 text-3xl hover:text-white"
+                />
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+      <div tw="text-xs">© Untitled. All rights reserved.</div>
     </footer>
   )
 }
