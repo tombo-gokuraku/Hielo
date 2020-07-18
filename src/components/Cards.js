95bdb163ca6c7450dc0604463710ff6323d66b11
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-07-18 10:03:15 +0900

    add button to Card component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,20 +3,24 @@ import "twin.macro"
 import Card from "./Card"
 import Heading from "./Heading"
 import Paragraph from "./Paragraph"
+import Button from "./Button"
 
 function Cards() {
   return (
     <Card>
-      <Heading
-        title="Lorem ipsum dolor"
-        desc="MAECENAS SAPIEN FEUGIAT EX PURUS"
-      />
-      <Paragraph>
-        Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis.
-        Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra
-        elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat
-        malesuada.
-      </Paragraph>
+      <div tw="flex flex-col items-center p-6 ">
+        <Heading
+          title="Lorem ipsum dolor"
+          desc="MAECENAS SAPIEN FEUGIAT EX PURUS"
+        />
+        <Paragraph>
+          Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis.
+          Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra
+          elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat
+          malesuada.
+        </Paragraph>
+        <Button>Learn More</Button>
+      </div>
     </Card>
   )
 }
