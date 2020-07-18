ed75602e641a12f15ce9aeec63523f81bb847bf2
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-07-18 10:52:08 +0900

    add card data

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,27 +1,66 @@
 import React from "react"
 import "twin.macro"
-import Card from "./Card"
 import Heading from "./Heading"
 import Paragraph from "./Paragraph"
 import Button from "./Button"
 
+import Image from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
+
+const query = graphql`
+  {
+    pic02: file(relativePath: { eq: "pic02.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 640) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    pic03: file(relativePath: { eq: "pic03.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 640) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
 function Cards() {
+  const data = useStaticQuery(query)
+  const cardData = [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor",
+      subTitle: "MAECENAS SAPIEN FEUGIAT EX PURUS",
+      description:
+        "Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat malesuada.",
+      image: data.pic02.childImageSharp.fluid,
+    },
+    {
+      id: 2,
+      title: "Vestibulum sit amet",
+      subTitle: "MATTIS ELEMENTUM SAPIEN PRETIUM TELLUS",
+      description:
+        "Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat malesuada.",
+      image: data.pic03.childImageSharp.fluid,
+    },
+  ]
   return (
-    <Card>
-      <div tw="flex flex-col items-center p-6 ">
-        <Heading
-          title="Lorem ipsum dolor"
-          desc="MAECENAS SAPIEN FEUGIAT EX PURUS"
-        />
-        <Paragraph>
-          Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis.
-          Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra
-          elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat
-          malesuada.
-        </Paragraph>
-        <Button>Learn More</Button>
-      </div>
-    </Card>
+    <section>
+      {cardData.map(card => {
+        return (
+          <div tw="w-full max-w-screen-sm" key={card.id}>
+            <Image fluid={card.image} tw="w-full" />
+            <div tw="flex flex-col items-center p-6 ">
+              <Heading title={card.title} desc={card.subTitle} />
+              <Paragraph>{card.description}</Paragraph>
+              <Button>Learn More</Button>
+            </div>
+          </div>
+        )
+      })}
+    </section>
   )
 }
 
