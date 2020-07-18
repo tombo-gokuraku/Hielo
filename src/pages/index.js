d34aa633aa8f3c1e2c144ab430660edec69f770e
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-07-18 10:02:37 +0900

    remove Button component from index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,11 @@ import SEO from "../components/seo"
 import Hero from "../components/Hero"
 import Cards from "../components/Cards"
 import SectionTitle from "../components/SectionTitle"
-import Button from "../components/Button"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero></Hero>
-    <Button>Learn More</Button>
     <Cards />
     <SectionTitle />
   </Layout>
